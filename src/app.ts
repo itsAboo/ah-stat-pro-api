aef36b6116b59d6a22cc6d7e0b7aa401d0b06013
itsAboo <mew.thanachot@gmail.com>
2025-07-30 17:38:52 +0700

    test

--- src/app.ts
+++ src/app.ts
@@ -37,6 +37,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 app.use("/api/user", userRoute);
 app.use("/api/handicap", handicapMovementRoute);
 app.use("/api/post", postRoute);
